ce64d00489d5ac18a3cc5794ccc8458450c0797a
mohhamedSamu <sfernandocalderon@gmail.com>
2026-07-10 23:29:27 -0600

    controles

--- scripts/ui/dev_overlay.gd
+++ scripts/ui/dev_overlay.gd
@@ -4,6 +4,9 @@ extends CanvasLayer
 
 var _fps_visible := false
 var _dev_hint_visible := false
+var _gamepad_fps_combo_was_pressed := false
+
+const _TRIGGER_THRESHOLD := 0.45
 
 
 func _ready() -> void:
@@ -25,11 +28,39 @@ func _unhandled_input(event: InputEvent) -> void:
 			get_viewport().set_input_as_handled()
 			return
 	if event.is_action_pressed("toggle_fps"):
-		_fps_visible = not _fps_visible
-		_fps_label.visible = _fps_visible
+		_set_fps_visible(not _fps_visible)
 		get_viewport().set_input_as_handled()
 
 
 func _process(_delta: float) -> void:
+	_update_gamepad_fps_combo()
 	if _fps_visible and not _dev_hint_visible:
 		_fps_label.text = "%d FPS" % int(Performance.get_monitor(Performance.TIME_FPS))
+
+
+func _update_gamepad_fps_combo() -> void:
+	if not InputHints.is_gamepad():
+		_gamepad_fps_combo_was_pressed = false
+		return
+	var combo_pressed := _is_all_shoulder_triggers_pressed()
+	if combo_pressed and not _gamepad_fps_combo_was_pressed:
+		_set_fps_visible(not _fps_visible)
+	_gamepad_fps_combo_was_pressed = combo_pressed
+
+
+func _is_all_shoulder_triggers_pressed() -> bool:
+	var pads := Input.get_connected_joypads()
+	if pads.is_empty():
+		return false
+	var device_id := int(pads[0])
+	var lb := Input.is_joy_button_pressed(device_id, JoyButton.JOY_BUTTON_LEFT_SHOULDER)
+	var rb := Input.is_joy_button_pressed(device_id, JoyButton.JOY_BUTTON_RIGHT_SHOULDER)
+	var lt := Input.get_joy_axis(device_id, JoyAxis.JOY_AXIS_TRIGGER_LEFT) > _TRIGGER_THRESHOLD
+	var rt := Input.get_joy_axis(device_id, JoyAxis.JOY_AXIS_TRIGGER_RIGHT) > _TRIGGER_THRESHOLD
+	return lb and rb and lt and rt
+
+
+func _set_fps_visible(visible: bool) -> void:
+	_fps_visible = visible
+	if not _dev_hint_visible:
+		_fps_label.visible = visible
